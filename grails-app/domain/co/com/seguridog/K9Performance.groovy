734fa213b70813849e48f8e664f937a23df379db
Jefersson <jfcanob@unal.edu.co>
2015-11-13 01:17:39 -0500

    Adding K9Performace

--- grails-app/domain/co/com/seguridog/K9Performance.groovy
+++ grails-app/domain/co/com/seguridog/K9Performance.groovy
@@ -1,39 +1,39 @@
 package co.com.seguridog
 
 class K9Performance {
-    String Sentado
-    String Echado
-    String Llamado
-    String Quieto
-    String CaminarAlLado
-    String ManejoTradilla
-    String Indicacion
-    String ControlCanino
-    String Entonacion
-    String PremiaAdecuadamente
-    String AlertarOrden
-    String Inmovilizar
-    String SoltarVigilar
-    String ElementosUsados
-    String Observaciones
-    String Recomendaciones
+    String sentado
+    String echado
+    String llamado
+    String quieto
+    String caminarAlLado
+    String manejoTradilla
+    String indicacion
+    String controlCanino
+    String entonacion
+    String premiaAdecuadamente
+    String alertarOrden
+    String inmovilizar
+    String soltarVigilar
+    String elementosUsados
+    String observaciones
+    String recomendaciones
 
     static constraints = {
-        Sentado (inList["SI", "NO"], blank: false)
-        Echado (inList["SI", "NO"], blank: false)
-        Llamado (inList["SI", "NO"], blank: false)
-        Quieto (inList["SI", "NO"], blank: false)
-        CaminarAlLado (inList["SI", "NO"], blank: false)
-        ManejoTradilla (inList["SI", "NO"], blank: false)
-        Indicacion (inList["SI", "NO"], blank: false)
-        ControlCanino (inList["SI", "NO"], blank: false)
-        Entonacion (inList["SI", "NO"], blank: false)
-        PremiaAdecuadamente (inList["SI", "NO"], blank: false)
-        AlertarOrden (inList["SI", "NO"], blank: false)
-        Inmovilizar (inList["SI", "NO"], blank: false)
-        SoltarVigilar (inList["SI", "NO"], blank: false)
-        ElementosUsados (blank: false)
-        Observaciones(blank: false)
-        Recomendaciones(blank: false)
+        sentado (inList:["SI", "NO"], blank: false)
+        echado (inList:["SI", "NO"], blank: false)
+        llamado (inList:["SI", "NO"], blank: false)
+        quieto (inList:["SI", "NO"], blank: false)
+        caminarAlLado (inList:["SI", "NO"], blank: false)
+        manejoTradilla (inList:["SI", "NO"], blank: false)
+        indicacion (inList:["SI", "NO"], blank: false)
+        controlCanino (inList:["SI", "NO"], blank: false)
+        entonacion (inList:["SI", "NO"], blank: false)
+        premiaAdecuadamente (inList:["SI", "NO"], blank: false)
+        alertarOrden (inList:["SI", "NO"], blank: false)
+        inmovilizar (inList:["SI", "NO"], blank: false)
+        soltarVigilar (inList:["SI", "NO"], blank: false)
+        elementosUsados (blank: false)
+        observaciones(blank: false)
+        recomendaciones(blank: false)
     }
 }
