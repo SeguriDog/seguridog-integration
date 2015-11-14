b67c3293e61a46ee52b03a1d5f12e22e9a60baea
Edwin Alexander <eabohorquezg@unal.edu.co>
2015-11-13 23:52:25 -0500

    Se valida que el usuario este habilitado

--- grails-app/controllers/co/com/seguridog/K9AdminController.groovy
+++ grails-app/controllers/co/com/seguridog/K9AdminController.groovy
@@ -8,9 +8,7 @@ class K9AdminController {
 
     def index() {}
 
-    def register() {
-
-    }
+    def register(){}
 
     def user_register = {
         redirect(controller: "K9Admin", action: "register")
@@ -21,39 +19,20 @@ class K9AdminController {
         [users:list]
     }
 
-    def save_data = {
-        def cedula = params.cedula
-        def firstName = params.firstName
-        def lastName = params.lastName
-        def loginUser = params.loginUser
-        def loginPass = params.loginPass
-        def birthDate = params.birthDate
-        def cellphone = params.cellphone
-        def eMail = params.eMail
-        def typeContract = params.typeContract
-        def dateContract = params.dateContract
-        def typeUsers = params.typeUsers
-
+    def save_data_user = {
         def nameClass
-        if( typeUsers.equals("2") ){
+        if( params.typeUsers.equals("2") ){
             nameClass = 'co.com.seguridog.K9Instructor'
+        }else if( params.typeUsers.equals("3") ){
+            nameClass = 'co.com.seguridog.K9Veterinarian'
         }else{
-            if( typeUsers.equals("3") ){
-                nameClass = 'co.com.seguridog.K9Veterinarian'
-            }else{
-                if( typeUsers.equals("4") ){
-                    nameClass = 'co.com.seguridog.K9Handler'
-                }
-            }
+            nameClass = 'co.com.seguridog.K9Handler'
         }
-
         Sql sql = Sql.newInstance(dataSource)
-        sql.execute('insert into k9user(version,birth_date,cedula,cellphone,date_contract,e_mail,first_name,last_name,login_pass,login_user,type_contract,type_users,class) values (?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?)',
-                [6,birthDate,cedula,cellphone,dateContract,eMail,firstName,lastName,loginPass,loginUser,typeContract,typeUsers,nameClass])
-        //se puede usar sql.executeInsert("insert into....")
+        sql.execute('insert into k9user(version,birth_date,cedula,cellphone,date_contract,e_mail,enable_user,first_name,last_name,login_pass,login_user,type_contract,type_users,class) values (?, ?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?)',
+                [6,params.birthDate,params.cedula,params.cellphone,params.dateContract,params.eMail,true,params.firstName,params.lastName,params.loginPass,params.loginUser,params.typeContract,params.typeUsers,nameClass])
         redirect(controller: "K9Admin", action: "index")
     }
-
 
     def register_service (){
         def handler_list = K9Handler.findAllByEnableUser(true)
