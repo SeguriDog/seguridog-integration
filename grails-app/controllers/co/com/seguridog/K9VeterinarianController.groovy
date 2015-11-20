330c59dfc48bc74826eb8f0432b4bc76eece755f
Mauricio Mejía Castro <jemmejiaca@unal.edu.co>
2015-11-20 11:52:17 -0500

    Se agrega actualizacion de hsitoria clinica (no termiando)

--- grails-app/controllers/co/com/seguridog/K9VeterinarianController.groovy
+++ grails-app/controllers/co/com/seguridog/K9VeterinarianController.groovy
@@ -16,10 +16,24 @@ class K9VeterinarianController {
         [canine_list : canine_list]
     }
 
+    def search_clinic_history_by_dog() {
+        //def canine_list = Canine.findAllByStateCanine('Activo')
+        def clinic_history_list = ClinicHistory.findAll()
+        [clinic_history_list : clinic_history_list]
+    }
+
+    def result_search_clinic_history() {
+
+    }
+
     def save_data_clinic() {
         Sql sql = Sql.newInstance(dataSource)
         sql.execute('insert into clinic_history(version,canines_id,date_treatment,evaluation,medics_id,name_treatment,type_treatment) values (?, ?, ?, ?, ?, ?, ?)',
                 [8,params.canine_id,params.dateTreatment,params.evaluation,session.user.id,params.nameTreatment,params.typeTreatment])
         redirect(controller: "K9Veterinarian", action: "index")
+    }
+
+    def edit_data_clinic() {
+
     }
 }
